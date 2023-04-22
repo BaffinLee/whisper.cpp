6de77b056fe6d5f481f5797ed8cb2cddf5c35489
Baffin Lee <baffinlee@gmail.com>
2023-04-23 07:18:32 +0800

    chore: refactor script

--- extra/deploy-wasm-pages.sh
+++ extra/deploy-wasm-pages.sh
@@ -15,17 +15,23 @@ ensure_command() {
   fi
 }
 
-# ensure_command "cmake"
-if ! command -v cmake > /dev/null 2>&1; then
-  url=https://github.com/Kitware/CMake/releases/download/v3.25.3/cmake-3.25.3-linux-x86_64.tar.gz
+download() {
   if command -v wget >/dev/null 2>&1; then
-    wget --quiet --show-progress -O cmake.tar.gz $url
+    wget --quiet --show-progress -O $2 $1
   elif command -v curl >/dev/null 2>&1; then
-    curl -L --output cmake.tar.gz $url
+    curl -L --output $2 $1
   else
     ensure_command "wget"
-    wget --quiet --show-progress -O cmake.tar.gz $url
+    wget --quiet --show-progress -O $2 $1
   fi
+}
+
+mkdir -p build-em/
+cd build-em/
+
+# install cmake for vercel
+if ! command -v cmake > /dev/null 2>&1; then
+  download "https://github.com/Kitware/CMake/releases/download/v3.25.3/cmake-3.25.3-linux-x86_64.tar.gz" "cmake.tar.gz"
   tar -xf cmake.tar.gz
   rm cmake.tar.gz
   chmod +x cmake-3.25.3-linux-x86_64/bin/*
@@ -33,43 +39,39 @@ if ! command -v cmake > /dev/null 2>&1; then
   echo $PATH
 fi
 
-mkdir -p build-em/
-cd build-em/
-
+# download emsdk
 if ! [ -d "emsdk-master" ] || ! [ -e "emsdk-master/emsdk_env.sh" ] || ! [ -e "emsdk-master/emsdk.py" ]; then
-  url=https://github.com/emscripten-core/emsdk/archive/master.tar.gz
-  if command -v wget >/dev/null 2>&1; then
-    wget --quiet --show-progress -O master.tar.gz $url
-  elif command -v curl >/dev/null 2>&1; then
-    curl -L --output master.tar.gz $url
-  else
-    ensure_command "wget"
-    wget --quiet --show-progress -O master.tar.gz $url
-  fi
+  download "https://github.com/emscripten-core/emsdk/archive/master.tar.gz" "master.tar.gz"
   ensure_command "xz"
   tar -xf master.tar.gz
   rm master.tar.gz
 fi
 
+# prepare emsdk env
 emsdk-master/emsdk update
 emsdk-master/emsdk install latest
 emsdk-master/emsdk activate latest
-
 source ./emsdk-master/emsdk_env.sh
 
-emcmake cmake ..
-make -j
+# build
+emcmake cmake .. && make -j
+if [ $? -ne 0 ]; then
+    echo "Error: build failed"
+    exit
+fi
 
 # copy files to public folder
-rm -rf public
-mkdir public
+mkdir -p public
 cp -r bin/whisper.wasm/* public/ && cp bin/libmain.worker.js public/
 mkdir -p public/stream && cp -r bin/stream.wasm/* public/stream/ && cp bin/libstream.worker.js public/stream/
 mkdir -p public/command && cp -r bin/command.wasm/* public/command/ && cp bin/libcommand.worker.js public/command/
 mkdir -p public/talk && cp -r bin/talk.wasm/* public/talk/ && cp bin/libtalk.worker.js public/talk/
 mkdir -p public/bench && cp -r bin/bench.wasm/* public/bench/ && cp bin/libbench.worker.js public/bench/
 
-# custom header config for netify/cloudflare pages
+# add custom header config for netify/cloudflare pages
 echo -e "/*\n  Cross-Origin-Embedder-Policy: require-corp\n  Cross-Origin-Opener-Policy: same-origin" > public/_headers
 
-echo -e "Done.\nOutput folder is: build-em/public"
+# done
+echo "Output folder is: build-em/public"
+echo "Done."
+exit
